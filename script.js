3af1580a43e193973cc05067034ff7c10edf39fd
Wei En <weien@Weis-Laptop-3.local>
2026-09-18 03:46:18 +0800

    Improve mobile responsiveness and navigation

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
+const menuIcon = document.querySelector('#menu-icon');
+const navLinks = document.querySelector('.nav-links');
+
+menuIcon.onclick = () => {
+    navLinks.classList.toggle('active');
+}
+
+navLinks.onclick = () => {
+    navLinks.classList.remove('active');
+}
+
 function copyEmail() {
     navigator.clipboard.writeText("tanweien2703@gmail.com");
     alert("Copied email.");
